f7219cb41a3c3403c117cbd684f50ad28d9fea07
Daniel Santoso <51028951+denaiels@users.noreply.github.com>
2021-04-07 17:06:12 +0700

    STORYBOARD

--- recycle-hero-app/recycle-hero-app/ViewController.swift
+++ recycle-hero-app/recycle-hero-app/ViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var recyledItem = [RecycledItemsModel]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
