de01a946674b65d56689afcb325a589b2b2d8974
Daniel Santoso <51028951+denaiels@users.noreply.github.com>
2021-04-08 16:53:08 +0700

    Finished Database

--- recycle-hero-app/recycle-hero-app/Playground1ViewController.swift
+++ recycle-hero-app/recycle-hero-app/Playground1ViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class Playground1ViewController: UIViewController {
 
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
--- recycle-hero-app/recycle-hero-app/RecycledItemsModel.swift
+++ recycle-hero-app/recycle-hero-app/RecycledItemsModel.swift
@@ -19,7 +19,48 @@ struct RecycledItemsModel {
     var item_found = false
 }
 
-struct Person {
-    var name = ""
-    var age = 0
-}
+var recycledItemNames = ["Compost", "Bottle Lamp", "Wool Fiber", "Plant Bottle", "Bird House", "Watering Jug", "Fertilizer", "Bottle Water Gun", "Surfboard", "Newspaper Kite", "Bottle Boat", "Paper Windmill"]
+var recycledItemStage = [1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 3]
+var recycledItemIngredient1 = [0, 5, 3, 5, 7, 9, 11, 5, 14, 1, 17, 19]
+var recycledItemIngredient2 = [1, 2, 4, 6, 8, 10, 12, 13, 15, 16, 18, 1]
+var recycledItemIconColor =
+    ["compost",
+     "bottleLamp",
+     "woolFiber",
+     "plantBottle",
+     "birdhouseCanPaint",
+     "wateringJug",
+     "fertilizer",
+     "bottleWaterGun",
+     "surfboard",
+     "newspaperKite",
+     "bottleBoat",
+     "paperWindmill"]
+var recycledItemDescription =
+    ["A recycled product from Food Scrap and Newspaper. You can make this at home with your Parents!",
+     "A recycled product from Plastic Bottle and Light Bulb. You can make this at home with your Parents!",
+     "A recycled product from Used Cloth. You can make this using scissors at home with your Parents guidance!",
+     "A recycled product from Plastic Bottle and Small Plant. You can make this at home with your Parents!",
+     "A recycled product from Paint Can and Ribbon. You can make this at home with your Parents!",
+     "A recycled product from Milk Jug. You can make this using Needle at home with your Parents guidance!",
+     "A recycled product from Banana Peels and Water. You can make this at home with your Parents!",
+     "A recycled product from Plastic Bottle and Water Sprayer. You can make this at home with your Parents!",
+     "A recycled product from Styrofoam. You can make this using saw at home with your Parents guidance!",
+     "A recycled product from Newspaper and Fallen Branches. You can make this at home with your Parents!",
+     "A recycled product from Piler of Water Bottle. You can make this using Tape at home with your Parents guidance!",
+     "A recycled product from Plastic Straw and Newspaper. You can make this at home with your Parents!"]
+var recycledItemLink =
+    ["https://www.youtube.com/watch?v=kA3q07paNbE",
+     "https://www.youtube.com/watch?v=Z_nySChubms",
+     "https://www.youtube.com/watch?v=7i0QMnz4ExY",
+     "https://onelittleproject.com/plastic-bottle-planters/",
+     "https://blog.gardenloversclub.com/gardens/recycled-gardens/",
+     "http://ajourneytoadream.blogspot.com/2012/03/helpful-yet-simple-diy-for-spring.html",
+     "https://www.stonefamilyfarmstead.com/banana-peel-fertilizer/?utm_content=bufferf7fd1&utm_medium=social&utm_source=facebook.com&utm_campaign=buffer",
+     "https://www.youtube.com/watch?v=Tqe-vFHGruI",
+     "https://www.youtube.com/watch?v=QmOoJjHRavE",
+     "https://www.youtube.com/watch?v=u8cI7EN97oU",
+     "https://www.youtube.com/watch?v=AAESdIGouZs",
+     "https://www.youtube.com/watch?v=sBLj1VP_stM"]
+var recycledItemFound = [false, false, false, false, false, false, false, false, false, false, false, false]
+
