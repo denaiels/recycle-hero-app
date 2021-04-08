86b60c2328e4f3ac777c348b87051c84c690a36f
LowSpecx <mauricetin.916@gmail.com>
2021-04-08 13:21:48 +0700

    Stage Menu Display done
    - belum auto layout
    - tap gesture belom ada function karena belom ada stage storyboard
    
    - Logic hidden udh ada tapi ga yakin

--- recycle-hero-app/recycle-hero-app/StageMenuController.swift
+++ recycle-hero-app/recycle-hero-app/StageMenuController.swift
@@ -7,7 +7,9 @@
 
 import UIKit
 
+
 class StageMenuController: UIViewController {
+    static var currentStage : Int? = nil
 
     @IBOutlet weak var bedroomView: UIView!
     
@@ -16,13 +18,40 @@ class StageMenuController: UIViewController {
     
     @IBOutlet weak var beachView: UIView!
     
+    
+    @IBOutlet weak var chooseStageLabel: UILabel!
+    
+    @IBOutlet weak var gardenLabel: UILabel!
+    
+    @IBOutlet weak var beachLabel: UILabel!
+    
+    @IBOutlet weak var gardenMask: UIImageView!
+    
+    @IBOutlet weak var beachMask: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        bedroomView.layer.cornerRadius = 10
-        gardenView.layer.cornerRadius = 10
-        beachView.layer.cornerRadius = 10
+        chooseStageLabel.addCharacterSpacing()
         
+        if let stage = StageMenuController.currentStage{
+            if stage==1{
+                gardenLabel.isHidden = true
+                beachLabel.isHidden = true
+                gardenMask.isHidden = false
+                beachMask.isHidden = false
+            }
+            
+            if stage==2{
+                gardenLabel.isHidden = false
+                gardenMask.isHidden = true
+            }
+            
+            if stage==3{
+                beachLabel.isHidden = false
+                beachMask.isHidden = true
+            }
+        }
         
         // Do any additional setup after loading the view.
     }
@@ -38,4 +67,15 @@ class StageMenuController: UIViewController {
     }
     */
 
+}
+
+extension UILabel{
+    func addCharacterSpacing(kernValue: Double = 1.15){
+        if let labelText = text, labelText.count > 0 {
+            let attributedString = NSMutableAttributedString(string: labelText)
+            attributedString.addAttribute(NSAttributedString.Key.kern, value: kernValue, range:NSRange(location: 0, length: attributedString.length-1))
+            
+            attributedText = attributedString
+        }
+    }
 }
