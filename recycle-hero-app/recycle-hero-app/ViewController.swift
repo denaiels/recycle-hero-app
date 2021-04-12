a38babe5df164848c4ff63bc462acb9b597920e5
Daniel Santoso <51028951+denaiels@users.noreply.github.com>
2021-04-12 14:22:50 +0700

    Fix delete

--- recycle-hero-app/recycle-hero-app/ViewController.swift
+++ recycle-hero-app/recycle-hero-app/ViewController.swift
@@ -8,13 +8,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    var recyledItem = [RecycledItemsModel]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        print("Hello world")
     }
 
 
